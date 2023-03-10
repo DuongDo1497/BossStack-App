7588efb2cefc54faa0edb600104aa463bd9fa4df
DuongDo1497 <113325868+DuongDo1497@users.noreply.github.com>
2023-03-10 16:51:55 +0700

    T2D

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -6,11 +6,59 @@ const routes: Routes = [
   {
     path: '',
     component: HomePage,
-  }
+    children: [
+      {
+        path: 'radio',
+        children: [
+          {
+            path: '',
+            loadChildren: () =>
+              import('../pages/radio/radio.module').then(
+                (m) => m.RadioPageModule
+              ),
+          },
+        ],
+      },
+      {
+        path: 'library',
+        children: [
+          {
+            path: '',
+            loadChildren: () =>
+              import('../pages/library/library.module').then(
+                (m) => m.LibraryPageModule
+              ),
+          },
+        ],
+      },
+      {
+        path: 'search',
+        children: [
+          {
+            path: '',
+            loadChildren: () =>
+              import('../pages/search/search.module').then(
+                (m) => m.SearchPageModule
+              ),
+          },
+        ],
+      },
+      {
+        path: '',
+        redirectTo: 'radio',
+        pathMatch: 'full',
+      },
+    ],
+  },
+  {
+    path: '',
+    redirectTo: 'radio',
+    pathMatch: 'full',
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class HomePageRoutingModule {}
